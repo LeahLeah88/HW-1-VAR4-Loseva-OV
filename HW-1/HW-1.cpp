6e33f075588e69fca9a2cddb2dc754c3c4246bcc
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-24 11:32:46 +0300

    Add project files.
    Add project files.

---
+++ HW-1/HW-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+void task1()
+{
+	double x;
+	std::cout << "x = ";
+	std::cin >> x;
+	double y;
+	std::cout << "y = ";
+	std::cin >> y;
+	double result;
+	result = ((sin(x) + cos(y)) / tan(x)) + 0, 43;
+	std::cout << std::setprecision(5) << std::fixed;
+	std::cout << "Result = " << result;
+}
+
+int main()
+{
+	task1();
+}
