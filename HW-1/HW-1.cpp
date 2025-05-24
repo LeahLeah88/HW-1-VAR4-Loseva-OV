3cb00705d872571d04759256e12cc09287804617
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-24 14:58:08 +0300

    Добавлена задача №3.
    Добавлена задача №3.

--- HW-1/HW-1.cpp
+++ HW-1/HW-1.cpp
@@ -28,9 +28,25 @@ void task2()
 	std::cout << std::setprecision(5) << std::fixed;
 	std::cout << std::setw(25) << "Result = " << result;
 	}
+void task3()
+{
+	setlocale(LC_ALL, "Russian");
 
+	int m;
+	int n;
+	std::cout << "m = ";
+	std::cin >> m;
+	std::cout << "n = ";
+	std::cin >> n;
+
+	if (m % n == 0)
+		std::cout << m / n;
+	else
+		std::cout << "M на N нацело не делится";
+}
 int main()
 {
 	task1();
 	task2();
+	task3();
 }
