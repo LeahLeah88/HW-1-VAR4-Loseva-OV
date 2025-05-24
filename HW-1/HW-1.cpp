23d647fc3c69366a5f66be174985af32045a12a5
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-24 12:06:35 +0300

    Добавлена задача №2.
    Добавлена задача №2.

--- HW-1/HW-1.cpp
+++ HW-1/HW-1.cpp
@@ -15,8 +15,22 @@ void task1()
 	std::cout << std::setprecision(5) << std::fixed;
 	std::cout << "Result = " << result;
 }
+void task2()
+	{
+		double x;
+	std::cout << std::setw(20) << "x = ";
+	std::cin >> x;
+	double y;
+	std::cout << std::setw(20) << "y = ";
+	std::cin >> y;
+	double result;
+	result = (pow(x, 3) + pow(y, 3)) / 2;
+	std::cout << std::setprecision(5) << std::fixed;
+	std::cout << std::setw(25) << "Result = " << result;
+	}
 
 int main()
 {
 	task1();
+	task2();
 }
